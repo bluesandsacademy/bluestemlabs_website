1162b424269bd1d84817b1770b129537b214a087
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-07-28 03:20:35 +0100

    chore: updated content

--- src/components/footers/IndexSectionFooters7.js
+++ src/components/footers/IndexSectionFooters7.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { socialMedia } from "../../data";
 
 export default function IndexSectionFooters7() {
   return (
@@ -15,32 +16,51 @@ export default function IndexSectionFooters7() {
               <h1 className="font-heading text-4xl xs:text-5xl md:text-6xl font-semibold text-white mb-20 removed">
                 <span>Connect with us</span>
               </h1>
-              <div className="sm:flex items-center">
-                <div className="sm:flex mb-6 sm:mb-0 sm:mr-12 items-center">
-                  <div className="flex mb-3 sm:mb-0 sm:mr-3 items-center justify-center w-12 h-12 bg-orange-900 rounded-full">
-                    <img
-                      src="saturn-assets/images/footers/map-pin-light.svg"
-                      alt
-                    />
+              <div className="sm:flex items-start justify-center">
+                <div className="flex flex-col gap-y-3 mb-6 sm:mb-0 sm:mr-12 items-center justify-start">
+                  <div className="">
+                    <p className="text-[#1980E7] text-xl font-bold">EDO STATE</p>
+                    <span className="block text-orange-50">
+                      No 7 Omokhale Street, Off Federal Government Girls College
+                      Road. UBTH Back Gate, Benin City.
+                    </span>
                   </div>
-                  <div className="removed">
-                    <p className="text-white">LAGOS STATE</p>
+                  <div className="">
+                    <p className="text-[#1980E7] text-xl font-bold">LAGOS STATE</p>
                     <span className="block text-orange-50">
                       House 10 Subuola Abu Street, ​Greenland Estate, Lagos
                       State.
                     </span>
                   </div>
                 </div>
-                <div className="sm:flex items-center">
-                  <div className="flex mb-3 sm:mb-0 sm:mr-3 items-center justify-center w-12 h-12 bg-orange-900 rounded-full">
-                    <img
-                      src="saturn-assets/images/footers/envelope-light.svg"
-                      alt
-                    />
-                  </div>
-                  <div>
+                <div className="flex flex-col items-start gap-y-3 justify-start">
+                  <div className="">
+                    <p className="text-[#1980E7] text-xl font-bold">EMAIL</p>
                     <span className="block text-orange-50">
-                      support@bluesandsstemlabs.com
+                      <a href="mailto:info@bluesandstemlabs.com">
+                        info@bluesandstemlabs.com
+                      </a>
+                    </span>
+                  </div>
+                  <div className="">
+                    <p className="text-[#1980E7] text-xl font-bold">PHONE NUMBER</p>
+                    <span className="block text-orange-50">
+                      <a href="tel:+234-905-493-0215">+234-905-493-0215</a>
+                    </span>
+                    <span className="block text-orange-50">
+                      <a href="tel:+234-703-419-4669">+234-703-419-4669</a>
+                    </span>
+                  </div>
+                  <div className="">
+                    <p className="text-[#1980E7] text-xl font-bold">SOCIAL MEDIA</p>
+                    <span className="inline-flex items-center justify-center text-orange-50">
+                      {socialMedia.map((media, index) => {
+                        return (
+                          <a href={media.link} key={index}>
+                            <media.icon className="stroke-[#1980E7]" />
+                          </a>
+                        );
+                      })}
                     </span>
                   </div>
                 </div>
--- src/data/index.js
+++ src/data/index.js
@@ -29,6 +29,67 @@ export const team = [
   },
 ];
 
-export const features = [
-  
-]
+export const features = [];
+
+export const socialMedia = [
+  {
+    link: "https://instagram.com/bluesandstemlabs",
+    icon: (props) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        {...props}
+      >
+        <path
+          fill="currentColor"
+          d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+        />
+      </svg>
+    ),
+  },
+  {
+    link: "https://www.tiktok.com/@bluesandstemlabs",
+    icon: (props) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        {...props}
+      >
+        <path
+          fill="currentColor"
+          d="M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 15.54 3h-3.09v12.4a2.592 2.592 0 0 1-2.59 2.5c-1.42 0-2.6-1.16-2.6-2.6c0-1.72 1.66-3.01 3.37-2.48V9.66c-3.45-.46-6.47 2.22-6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69-2.55 5.69-5.7V9.01a7.35 7.35 0 0 0 4.3 1.38V7.3s-1.88.09-3.24-1.48"
+        />
+      </svg>
+    ),
+  },
+  {
+    link: "https://x.com/bsa_stemlabs",
+    icon: (props) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 14 14"
+        {...props}
+      >
+        <g fill="none">
+          <g clip-path="url(#primeTwitter0)">
+            <path
+              fill="currentColor"
+              d="M11.025.656h2.147L8.482 6.03L14 13.344H9.68L6.294 8.909l-3.87 4.435H.275l5.016-5.75L0 .657h4.43L7.486 4.71zm-.755 11.4h1.19L3.78 1.877H2.504z"
+            />
+          </g>
+          <defs>
+            <clipPath id="primeTwitter0">
+              <path fill="#fff" d="M0 0h14v14H0z" />
+            </clipPath>
+          </defs>
+        </g>
+      </svg>
+    ),
+  },
+];
